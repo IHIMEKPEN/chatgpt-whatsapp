bff5491577b2cf2e648e001588e0838f3a6f9f26
Ihimekpen Osemudiamen Andrew <oihimekpen@gmail.com>
2023-10-18 10:53:29 +0100

    first commit

---
+++ src/configs/constant.ts
@@ -0,0 +1,45 @@
+export const APP_NAME = `Andre's BOT`;
+export const PREFIXES = [
+  'Zappy',
+  'ZappyBot',
+  'Zappy-Bot',
+  'Zappy Bot',
+  'zappy',
+  'zappybot',
+  'zappy-bot',
+  'zappy bot',
+  'gpt',
+  'GPT',
+  'gpt3',
+  'GPT3',
+  'gpt-3',
+  'GPT-3',
+  'bot',
+  'Bot',
+  'BOT',
+  'zappy,',
+  'Zappy,',
+  'ZAPPY,',
+  'zappy?',
+  'Zappy?',
+  'ZAPPY?',
+  'zappy!',
+];
+
+export const REMOVABLE_PREFIXES = [
+  'Zappy',
+  'ZappyBot',
+  'Zappy-Bot',
+  'Zappy Bot',
+  'zappy',
+  'zappybot',
+  'zappy-bot',
+  'zappy bot',
+  'zappy,',
+  'Zappy,',
+  'ZAPPY,',
+];
+
+export const OPENAI_MODEL = 'gpt-3.5-turbo';
+
+export const DEFAULT_SYSTEM_MESSAGE = `You are ChatGPT, a large language model trained by OpenAI. Answer as concisely as possible.\nCurrent date: ${new Date().toISOString()}`;
